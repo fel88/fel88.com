aa4373ee0d3e1e8742ebac52e3a8a37d14a8c53c
fel88 <15663687+fel88@users.noreply.github.com>
2026-06-11 14:34:59 +0300

    +stars horizontal

--- Codes/Samples/Stars_Horizontal/Form1.cs
+++ Codes/Samples/Stars_Horizontal/Form1.cs
@@ -1,3 +1,8 @@
+using System.Drawing;
+using System.Security.Policy;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
 namespace Stars_Horizontal
 {
     public partial class Form1 : Form
@@ -5,6 +10,57 @@ namespace Stars_Horizontal
         public Form1()
         {
             InitializeComponent();
+            Paint += Form1_Paint;
+            InitStars();
+            SizeChanged += Form1_SizeChanged;
+            timer.Tick += (sender, e) =>
+            {                
+                    Invalidate();
+            };
+            timer.Interval = 1;
+            timer.Start();
+            DoubleBuffered = true;
+        }
+
+        private void Form1_SizeChanged(object? sender, EventArgs e)
+        {
+            InitStars();
+        }
+
+        Timer timer = new Timer();
+
+        Random rand = new Random();
+        private void InitStars()
+        {
+            var width = ClientSize.Width;
+            var height = ClientSize.Height;
+            for (int i = 0; i < N; i++)
+            {
+                starsX[i] = rand.Next(width);
+                starsY[i] = rand.Next(height);
+                starsSpeed[i] = rand.Next(10, 100) / 10f;
+                starsSize[i] = rand.Next(1, 4);
+            }
+        }
+
+        const int N = 400;
+
+        float[] starsX = new float[N];
+        float[] starsY = new float[N];
+        float[] starsSize = new float[N];
+        float[] starsSpeed = new float[N];
+
+        
+        private void Form1_Paint(object? sender, PaintEventArgs e)
+        {            
+            var gr = e.Graphics;
+            gr.Clear(Color.Black);
+            for (int i = 0; i < N; i++)
+            {
+                gr.FillEllipse(Brushes.Silver, starsX[i], starsY[i], starsSize[i], starsSize[i]);
+                starsX[i] -= starsSpeed[i];
+                if (starsX[i] < 0) starsX[i] = Width;
+            }
         }
     }
 }
