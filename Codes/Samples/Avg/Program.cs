3fe9c1729ce96112bda3651f3ed600aecd67ec08
fel88 <15663687+fel88@users.noreply.github.com>
2026-06-20 09:41:25 +0300

    upd

---
+++ Codes/Samples/Avg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] data = new int[N]; // создаем массив чисел длиной N, где будем запоминать введенные пользователм числа
+
+// считываем N чисел
+for (int i = 0; i < N; i++)
+{
+    data[i] = int.Parse(Console.ReadLine());     // записываем очередное число в массив
+}
+
+// расчитаем среднее пройдясь по массиву и накопив sum 
+int sum = 0; // аккумулятор суммы
+for (int i = 0; i < N; i++)
+{
+    sum += data[i];
+}
+
+// вычислим среднее
+double avg = sum / (double)N;
+Console.WriteLine($"Среднее = {avg}");
+//пройдемся еще раз по массиву и будем выводить числа, которые больше или равны среднему
+Console.WriteLine("Числа больше среднего: ");
+for (int i = 0; i < N; i++)
+{
+    if (data[i] >= avg)
+        Console.WriteLine(data[i]);
+}
