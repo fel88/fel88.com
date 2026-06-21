317c1d97250be9ce3d95c3f557b873bb106b396a
fel88 <15663687+fel88@users.noreply.github.com>
2026-06-21 08:00:45 +0300

    upd

--- Codes/Samples/Gradient/Form1.Designer.cs
+++ Codes/Samples/Gradient/Form1.Designer.cs
@@ -28,34 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(684, 511);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(684, 511);
-            Controls.Add(pictureBox1);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "fel88.com - gradient";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
     }
 }
--- Codes/Samples/Gradient/Form1.cs
+++ Codes/Samples/Gradient/Form1.cs
@@ -5,42 +5,63 @@ namespace Gradient
         public Form1()
         {
             InitializeComponent();
-            DrawHorizontalGradient();
+            DoubleBuffered = true;
+            Paint += Form1_Paint;
+            bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
+            Init();
         }
 
-        public void DrawHorizontalGradient()
+        public void Init()
         {
-            Bitmap bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
+            DrawHorizontalGradient(Color.DeepSkyBlue);
+        }
+
+        private void Form1_Paint(object? sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(bmp, 0, 0);
+        }
+
+        Bitmap bmp;
+        public void DrawHorizontalGradient(Color color)
+        {
             for (int i = 0; i < ClientSize.Width; i++)
             {
                 double koef = (i) / (double)ClientSize.Width;
-                Color color = Color.FromArgb((int)(koef * 255), 0, 0);
+
+                Color _color = Color.FromArgb((int)(koef * color.R), (int)(koef * color.G), (int)(koef * color.B));
+                for (int j = 0; j < ClientSize.Height; j++)
+                {
+                    bmp.SetPixel(i, j, _color);
+                }
+            }
+        }
+
+        public void Fill(Color color)
+        {
+            for (int i = 0; i < ClientSize.Width; i++)
+            {
                 for (int j = 0; j < ClientSize.Height; j++)
                 {
                     bmp.SetPixel(i, j, color);
                 }
             }
-            pictureBox1.Image = bmp;
         }
 
-        public void DrawVerticalGradient()
+        public void DrawVerticalGradient(Color color1)
         {
-            Bitmap bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
             for (int i = 0; i < ClientSize.Height; i++)
             {
                 double koef = (i) / (double)ClientSize.Height;
-                Color color = Color.FromArgb(0, (int)(koef * 255), 0);
+                Color color = Color.FromArgb((int)(koef * color1.R), (int)(koef * color1.G), (int)(koef * color1.B));
                 for (int j = 0; j < ClientSize.Width; j++)
                 {
                     bmp.SetPixel(j, i, color);
                 }
             }
-            pictureBox1.Image = bmp;
         }
 
         public void DrawDiagGradient()
         {
-            Bitmap bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
             for (int i = 0; i < ClientSize.Height; i++)
             {
                 for (int j = 0; j < ClientSize.Width; j++)
@@ -50,7 +71,36 @@ namespace Gradient
                     bmp.SetPixel(j, i, color);
                 }
             }
-            pictureBox1.Image = bmp;
         }
+
+
+        public void DrawRadialGradient(Color color)
+        {
+            for (int i = 0; i < ClientSize.Height; i++)
+            {
+                for (int j = 0; j < ClientSize.Width; j++)
+                {
+                    var xc = i - ClientRectangle.Height / 2;
+                    var yc = j - ClientRectangle.Width / 2;
+
+                    var dist = Math.Sqrt(xc * xc + yc * yc);
+
+                    var farPoint = Math.Sqrt(Math.Pow(ClientRectangle.Width / 2, 2) + Math.Pow(ClientRectangle.Height / 2, 2));
+                    var val = dist / farPoint;
+
+                    val = 1.0 - val;
+                    
+                    double r = color.R * val;
+                    double g = color.G * val;
+                    double b = color.B * val;
+
+                    Color _color = Color.FromArgb((int)r, (int)g, (int)b);
+
+                    bmp.SetPixel(j, i, _color);
+                }
+            }
+        }
+
+
     }
 }
